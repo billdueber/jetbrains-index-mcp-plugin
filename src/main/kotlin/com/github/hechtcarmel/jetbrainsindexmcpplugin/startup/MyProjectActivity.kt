0a7b5211f2dfc0fc19ad5b1d4a1778f88ebcf765
GitHub Action <action@github.com>
2025-11-25 07:43:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/hechtcarmel/jetbrainsindexmcpplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/hechtcarmel/jetbrainsindexmcpplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.hechtcarmel.jetbrainsindexmcpplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
